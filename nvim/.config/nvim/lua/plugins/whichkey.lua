a48a837a160ea9328724342e2e133a9a53856fe0
Niels Houben <niels.houben1@gmail.com>
2025-08-02 17:58:21 +0200

    feat: add diagnostics and actions buttons

--- nvim/.config/nvim/lua/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/plugins/whichkey.lua
@@ -6,6 +6,8 @@ return {
 
     wk.register({
       f = { function() vim.lsp.buf.format() end, "Format Buffer" },
+      d = { function() vim.diagnostic.open_float() end, "Show Diagnostics" },
+      a = { function() vim.lsp.buf.code_action() end, "Code Actions" },
     }, { prefix = "<leader>" })
   end,
 }

50634ee56f3584ca2ca87fd8b1cba70a7e377da8
Niels Houben <niels.houben1@gmail.com>
2025-08-02 18:10:52 +0200

    cleanup: remove duplicated lspconfig

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -51,16 +51,3 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     vim.lsp.buf.format({ bufnr = bufnr })
   end,
 })
-
-local lspconfig = require("lspconfig")
-
-lspconfig.rust_analyzer.setup({
-  on_attach = on_attach,
-  settings = {
-    ["rust-analyzer"] = {
-      check = {
-        command = "clippy",
-      },
-    },
-  },
-})
